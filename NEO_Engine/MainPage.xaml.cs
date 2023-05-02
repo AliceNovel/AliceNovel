b2ba9d35e0313cbe378728e12334cb832743ca57
Lemon73 <108001913+Lemon73-Computing@users.noreply.github.com>
2023-05-02 22:25:20 +0900

    基本UI・縦横に比率でUI変更(作成中)
    縦横の使わないほうのUIを消して、使うほうだけ表示する方法で実装しているが、horizontal optionsやvertical optionsがstartやendになっているものが勝手にcenterになったり、やたらと数値指定が面倒なので、他のより良い方法に変更予定

--- NEO_Engine/MainPage.xaml.cs
+++ NEO_Engine/MainPage.xaml.cs
@@ -2,13 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	//int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+    }
 
+	/*
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
@@ -20,5 +21,53 @@ public partial class MainPage : ContentPage
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
-}
+	*/
 
+    private void Main_ui_SizeChanged(object sender, EventArgs e)
+    {
+        double height = main_ui.Height;
+        double width = main_ui.Width;
+
+		if (height > width)
+		{
+			vertical_desigin.HeightRequest = 0;
+			vertical_desigin.WidthRequest = 0;
+			button1.WidthRequest = 0;
+			button2.WidthRequest = 0;
+			button3.WidthRequest = 0;
+			button4.WidthRequest = 0;
+			button5.WidthRequest = 0;
+			button6.WidthRequest = 0;
+
+            horizontal_desigin.HeightRequest = 400;
+            horizontal_desigin.WidthRequest = 400;
+            button1x.WidthRequest = 250;
+            button2x.WidthRequest = 250;
+            button3x.WidthRequest = 250;
+            button4x.WidthRequest = 250;
+            button5x.WidthRequest = 250;
+            button6x.WidthRequest = 250;
+        }
+		else
+		{
+
+            vertical_desigin.HeightRequest = 400;
+            vertical_desigin.WidthRequest = 400;
+            button1x.WidthRequest = 0;
+            button2x.WidthRequest = 0;
+            button3x.WidthRequest = 0;
+            button4x.WidthRequest = 0;
+            button5x.WidthRequest = 0;
+            button6x.WidthRequest = 0;
+
+            horizontal_desigin.HeightRequest = 0;
+            horizontal_desigin.WidthRequest = 0;
+            button1.WidthRequest = 250;
+            button2.WidthRequest = 250;
+            button3.WidthRequest = 250;
+            button4.WidthRequest = 250;
+            button5.WidthRequest = 250;
+            button6.WidthRequest = 250;
+        }
+    }
+}
