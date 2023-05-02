03f6e70c28897e07323eabd65c72a4dc08846ddf
Lemon73 <108001913+Lemon73-Computing@users.noreply.github.com>
2023-05-02 23:38:46 +0900

    縦横UI変更が可能に

--- NEO_Engine/MainPage.xaml.cs
+++ NEO_Engine/MainPage.xaml.cs
@@ -30,6 +30,36 @@ public partial class MainPage : ContentPage
 
 		if (height > width)
 		{
+            //縦UI
+            //row_desigin.Height = 0;
+            //column_desigin.Width = 500;
+            image_desigin.HorizontalOptions = LayoutOptions.Center;
+            image_desigin.VerticalOptions = LayoutOptions.Start;
+
+            vertical_desigin.IsVisible = false;
+            horizontal_desigin.IsVisible = true;
+
+            /*
+            button1.IsVisible = false;
+            button2.IsVisible = false;
+            button3.IsVisible = false;
+            button4.IsVisible = false;
+            button5.IsVisible = false;
+            button6.IsVisible = false;
+
+            button1x.IsVisible = true;
+            button2x.IsVisible = true;
+            button3x.IsVisible = true;
+            button4x.IsVisible = true;
+            button5x.IsVisible = true;
+            button6x.IsVisible = true;
+            */
+
+            //画像サイズ
+            image.HeightRequest = width - 40;
+            image.WidthRequest = width - 40;
+
+            /*
 			vertical_desigin.HeightRequest = 0;
 			vertical_desigin.WidthRequest = 0;
 			button1.WidthRequest = 0;
@@ -47,10 +77,40 @@ public partial class MainPage : ContentPage
             button4x.WidthRequest = 250;
             button5x.WidthRequest = 250;
             button6x.WidthRequest = 250;
+            */
         }
 		else
 		{
+            //横UI
+            //column_desigin.Width = 0;
+            //row_desigin.Height = 500;
+            image_desigin.HorizontalOptions = LayoutOptions.Start;
+            image_desigin.VerticalOptions = LayoutOptions.Center;
 
+            vertical_desigin.IsVisible = true;
+            horizontal_desigin.IsVisible = false;
+
+            /*
+            button1.IsVisible = true;
+            button2.IsVisible = true;
+            button3.IsVisible = true;
+            button4.IsVisible = true;
+            button5.IsVisible = true;
+            button6.IsVisible = true;
+
+            button1x.IsVisible = false;
+            button2x.IsVisible = false;
+            button3x.IsVisible = false;
+            button4x.IsVisible = false;
+            button5x.IsVisible = false;
+            button6x.IsVisible = false;
+            */
+
+            //画像サイズ
+            image.HeightRequest = height - 40;
+            image.WidthRequest = height - 40;
+
+            /*
             vertical_desigin.HeightRequest = 400;
             vertical_desigin.WidthRequest = 400;
             button1x.WidthRequest = 0;
@@ -68,6 +128,7 @@ public partial class MainPage : ContentPage
             button4.WidthRequest = 250;
             button5.WidthRequest = 250;
             button6.WidthRequest = 250;
+            */
         }
     }
 }
