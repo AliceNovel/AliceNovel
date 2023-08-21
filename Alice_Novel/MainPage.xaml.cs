a593429e23463dc8a39e23d04a2537d749c34261
Lemon73 <Lemonade.flash.1164@gmail.com>
2023-08-21 21:06:08 +0900

    ファイル保存系(試験段階)

--- Alice_Novel/MainPage.xaml.cs
+++ Alice_Novel/MainPage.xaml.cs
@@ -13,6 +13,41 @@ public partial class MainPage : ContentPage
 
     private async void Develop_Clicked(object sender, EventArgs e)
     {
+        //(DevelopPageへ移動処理)
+        await PickFolder(CancellationToken.None);
+        //await SaveFile(CancellationToken.None);
+
+        static async Task PickFolder(CancellationToken cancellationToken)
+        {
+            var result = await FolderPicker.Default.PickAsync(cancellationToken);
+            if (result.IsSuccessful)
+            {
+                //await Toast.Make($"フォルダーを確認しました。名称:{result.Folder.Name}, 場所:{result.Folder.Path}", ToastDuration.Long).Show(cancellationToken);
+                string location = result.Folder.Path;
+                //ここでDevelopPageへ移動処理
+            }
+            /*
+            else
+            {
+                await Toast.Make($"予期せぬエラーが発生しました。エラー内容: {result.Exception.Message}").Show(cancellationToken);
+            }
+            */
+        }
+        /*
+        async Task SaveFile(CancellationToken cancellationToken)
+        {
+            using var stream = new MemoryStream(Encoding.Default.GetBytes("Hello from the Community Toolkit!"));
+            var fileSaverResult = await FileSaver.Default.SaveAsync("test.txt", stream, cancellationToken);
+            if (fileSaverResult.IsSuccessful)
+            {
+                await Toast.Make($"The file was saved successfully to location: {fileSaverResult.FilePath}").Show(cancellationToken);
+            }
+            else
+            {
+                await Toast.Make($"The file was not saved successfully with error: {fileSaverResult.Exception.Message}").Show(cancellationToken);
+            }
+        }
+        */
     }
 
     private async void Setting_Clicked(object sender, EventArgs e)
