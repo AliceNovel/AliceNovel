ec5eb462af9fb1f6ea31ec997fb62dd6b47a5bda
Lemon73 <Lemonade.flash.1164@gmail.com>
2023-08-21 11:44:54 +0900

    MainPage -> GamePage

---
+++ Alice_Novel/GamePage.xaml.cs
@@ -0,0 +1,59 @@
+namespace Alice_Novel;
+
+public partial class GamePage : ContentPage
+{
+	public GamePage()
+	{
+		InitializeComponent();
+	}
+
+    //画面の比率によって画像の中心位置変更
+    private void game_ui_SizeChanged(object sender, EventArgs e)
+    {
+        /*
+        double height = main_ui.Height;
+        double width = main_ui.Width;
+
+        if (height > width)
+        {
+            //縦UI
+            image.VerticalOptions = LayoutOptions.Start;
+        }
+        else
+        {
+            //横UI
+            image.VerticalOptions = LayoutOptions.Center;
+        }
+        */
+    }
+
+    private void button1_Clicked(object sender, EventArgs e)
+    {
+        //button1またはbutton1xをクリックしたときの処理
+    }
+
+    private void button2_Clicked(object sender, EventArgs e)
+    {
+        //button2またはbutton2xをクリックしたときの処理
+    }
+
+    private void button3_Clicked(object sender, EventArgs e)
+    {
+        //button3またはbutton3xをクリックしたときの処理
+    }
+
+    private void button4_Clicked(object sender, EventArgs e)
+    {
+        //button4またはbutton4xをクリックしたときの処理
+    }
+
+    private void button5_Clicked(object sender, EventArgs e)
+    {
+        //button5またはbutton5xをクリックしたときの処理
+    }
+
+    private void button6_Clicked(object sender, EventArgs e)
+    {
+        //button6またはbutton6xをクリックしたときの処理
+    }
+}
--- Alice_Novel/MainPage.xaml.cs
+++ Alice_Novel/MainPage.xaml.cs
@@ -7,53 +7,8 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    //画面の比率によって画像の中心位置変更
-    private void Main_ui_SizeChanged(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
-        /*
-        double height = main_ui.Height;
-        double width = main_ui.Width;
-
-        if (height > width)
-        {
-            //縦UI
-            image.VerticalOptions = LayoutOptions.Start;
-        }
-        else
-        {
-            //横UI
-            image.VerticalOptions = LayoutOptions.Center;
-        }
-        */
-    }
-
-    private void button1_Clicked(object sender, EventArgs e)
-    {
-        //button1またはbutton1xをクリックしたときの処理
-    }
-
-    private void button2_Clicked(object sender, EventArgs e)
-    {
-        //button2またはbutton2xをクリックしたときの処理
-    }
-
-    private void button3_Clicked(object sender, EventArgs e)
-    {
-        //button3またはbutton3xをクリックしたときの処理
-    }
-
-    private void button4_Clicked(object sender, EventArgs e)
-    {
-        //button4またはbutton4xをクリックしたときの処理
-    }
-
-    private void button5_Clicked(object sender, EventArgs e)
-    {
-        //button5またはbutton5xをクリックしたときの処理
-    }
-
-    private void button6_Clicked(object sender, EventArgs e)
-    {
-        //button6またはbutton6xをクリックしたときの処理
+        await Shell.Current.GoToAsync("//GamePage");
     }
 }
