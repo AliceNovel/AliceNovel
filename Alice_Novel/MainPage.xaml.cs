6091e9678c9d407a23ad49ea79602311195ee89f
Lemon73 <Lemonade.flash.1164@gmail.com>
2023-08-22 11:40:51 +0900

    不要なファイル指定部分を削除

--- Alice_Novel/MainPage.xaml.cs
+++ Alice_Novel/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System.Text;
-using CommunityToolkit.Maui.Alerts;
-using CommunityToolkit.Maui.Core;
-using CommunityToolkit.Maui.Storage;
-
-namespace Alice_Novel;
+﻿namespace Alice_Novel;
 public partial class MainPage : ContentPage
 {
     public MainPage()
@@ -19,41 +14,6 @@ public partial class MainPage : ContentPage
     private async void Develop_Clicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//DevPage");//画面遷移
-
-        //(DevelopPageへ移動処理)
-        await PickFolder(CancellationToken.None);
-        //await SaveFile(CancellationToken.None);
-
-        static async Task PickFolder(CancellationToken cancellationToken)
-        {
-            var result = await FolderPicker.Default.PickAsync(cancellationToken);
-            if (result.IsSuccessful)
-            {
-                //await Toast.Make($"フォルダーを確認しました。名称:{result.Folder.Name}, 場所:{result.Folder.Path}", ToastDuration.Long).Show(cancellationToken);
-                string location = result.Folder.Path;
-            }
-            /*
-            else
-            {
-                await Toast.Make($"予期せぬエラーが発生しました。エラー内容: {result.Exception.Message}").Show(cancellationToken);
-            }
-            */
-        }
-        /*
-        async Task SaveFile(CancellationToken cancellationToken)
-        {
-            using var stream = new MemoryStream(Encoding.Default.GetBytes("Hello from the Community Toolkit!"));
-            var fileSaverResult = await FileSaver.Default.SaveAsync("test.txt", stream, cancellationToken);
-            if (fileSaverResult.IsSuccessful)
-            {
-                await Toast.Make($"The file was saved successfully to location: {fileSaverResult.FilePath}").Show(cancellationToken);
-            }
-            else
-            {
-                await Toast.Make($"The file was not saved successfully with error: {fileSaverResult.Exception.Message}").Show(cancellationToken);
-            }
-        }
-        */
     }
 
     private async void Setting_Clicked(object sender, EventArgs e)
