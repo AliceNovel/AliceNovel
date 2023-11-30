f308197838692509adb090c703f22cc6323ddb45
Lemon73 <108001913+Lemon73-Computing@users.noreply.github.com>
2023-11-30 14:08:40 +0900

    remove: content in mainpage

--- Alice_Novel/MainPage.xaml.cs
+++ Alice_Novel/MainPage.xaml.cs
@@ -1,18 +1,9 @@
 ﻿namespace Alice_Novel;
+
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();//ページの読み込み
-    }
-
-    private async void Game_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("//GamePage");//画面遷移
-    }
-
-    private async void Setting_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("//SettingPage");//画面遷移
     }
 }
