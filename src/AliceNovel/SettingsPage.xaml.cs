28b04163a4adc22cf5c1953495ab64d91bcce878
Lemon73 <Lemonade.flash.1164@gmail.com>
2024-10-31 19:07:45 +0900

    feat(app theme): add button to switch app theme

--- src/AliceNovel/SettingsPage.xaml.cs
+++ src/AliceNovel/SettingsPage.xaml.cs
@@ -5,5 +5,19 @@ public partial class SettingsPage : ContentPage
 	public SettingsPage()
 	{
 		InitializeComponent();
+
+        // Check Default AppTheme
+        if (Application.Current.RequestedTheme == AppTheme.Light)
+            switchThemeToLight.IsChecked = true;
+        else if (Application.Current.RequestedTheme == AppTheme.Dark)
+            switchThemeToDark.IsChecked = true;
 	}
+
+    private void SwitchTheme(object sender, CheckedChangedEventArgs e)
+    {
+        if (switchThemeToLight.IsChecked)
+            Application.Current.UserAppTheme = AppTheme.Light;
+        else if (switchThemeToDark.IsChecked)
+            Application.Current.UserAppTheme = AppTheme.Dark;
+    }
 }
