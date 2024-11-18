85127585e625045b56f5d29c082eb681d60f505d
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-11-18 21:13:51 +0900

    refactor: change files structure

--- src/AliceNovel/App.xaml.cs
+++ src/AliceNovel/App.xaml.cs
@@ -1,32 +1,30 @@
-﻿namespace AliceNovel
+﻿namespace AliceNovel;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public App()
     {
-        public App()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            // Set Application language (from Local Data)
-            string userAppLanguage = Preferences.Default.Get("AppLanguage", "Default");
-            if (userAppLanguage != "Default")
-                System.Globalization.CultureInfo.CurrentUICulture = new(userAppLanguage);
+        // Set Application language (from Local Data)
+        string userAppLanguage = Preferences.Default.Get("AppLanguage", "Default");
+        if (userAppLanguage != "Default")
+            System.Globalization.CultureInfo.CurrentUICulture = new(userAppLanguage);
 
-            MainPage = new AppShell();
+        // Set Application Theme (from Local Data)
+        string userAppTheme = Preferences.Default.Get("AppTheme", "Default");
+        if (userAppTheme == "Light")
+            Current.UserAppTheme = AppTheme.Light;
+        else if (userAppTheme == "Dark")
+            Current.UserAppTheme = AppTheme.Dark;
+    }
 
-            // Set Application Theme (from Local Data)
-            string userAppTheme = Preferences.Default.Get("AppTheme", "Default");
-            if (userAppTheme == "Light")
-                Current.UserAppTheme = AppTheme.Light;
-            else if (userAppTheme == "Dark")
-                Current.UserAppTheme = AppTheme.Dark;
-        }
-
-        protected override Window CreateWindow(IActivationState activationState) {
-            var window = base.CreateWindow(activationState);
-            // if (DeviceInfo.Current.Platform == DevicePlatform.WinUI)
-            //     window.Title = System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
-            window.Title = "Alice Novel";
-            return window;
-        }
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = new Window(new AppShell());
+        // if (DeviceInfo.Current.Platform == DevicePlatform.WinUI)
+        //     window.Title = System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
+        window.Title = "Alice Novel";
+        return window;
     }
 }
