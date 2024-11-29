ddc4f2ae2858fdec7fdada4a9503221b387f4752
Lemon73 <Lemonade.flash.1164@gmail.com>
2024-11-29 13:27:08 +0900

    feat(file-drop): show the origin of the dropped file

--- src/AliceNovel/MainPage.xaml.cs
+++ src/AliceNovel/MainPage.xaml.cs
@@ -3,6 +3,14 @@ using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.IO.Compression;
 using AliceNovel.Resources.Strings;
+#if WINDOWS
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+#elif IOS || MACCATALYST
+using UIKit;
+using Foundation;
+using System.Diagnostics;
+#endif
 // using CommunityToolkit.Maui.Alerts;
 
 namespace AliceNovel;
@@ -69,14 +77,68 @@ public partial class MainPage : ContentPage
 		ExitGame();
 	}
 
-  /// <summary>
-  /// The function when droped .anproj file into this app.
-	/// </summary>
-	/// <param name="sender"></param>
-	/// <param name="e"></param>
-    async private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+    /// <summary>
+    /// The function when droped .anproj file into this app.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+	#if WINDOWS || IOS || MACCATALYST
+    async private void OnDrop(object sender, DropEventArgs e)
+	#else
+    private void OnDrop(object sender, DropEventArgs e)
+	#endif
     {
-        textbox.Text = await e.Data.GetTextAsync();
+        var filePaths = new List<string>();
+
+		#if WINDOWS
+        if (e.PlatformArgs is not null && e.PlatformArgs.DragEventArgs.DataView.Contains(StandardDataFormats.StorageItems))
+        {
+            var items = await e.PlatformArgs.DragEventArgs.DataView.GetStorageItemsAsync();
+            if (items.Any())
+            {
+                foreach (var item in items)
+                {
+                    if (item is StorageFile file)
+                        filePaths.Add(item.Path);
+                }
+            }
+        }
+		#elif IOS || MACCATALYST
+		var session = e.PlatformArgs?.DropSession;
+		if (session == null)
+			return;
+
+		foreach (UIDragItem item in session.Items)
+		{
+			var result = await LoadItemAsync(item.ItemProvider, item.ItemProvider.RegisteredTypeIdentifiers.ToList());
+			if (result is not null)
+				filePaths.Add(result.FileUrl?.Path!);
+		}
+		foreach (var item in filePaths)
+		{
+			Debug.WriteLine($"Path: {item}");
+		}
+
+		static async Task<LoadInPlaceResult> LoadItemAsync(NSItemProvider itemProvider, List<string> typeIdentifiers)
+		{
+			if (typeIdentifiers is null || typeIdentifiers.Count == 0)
+				return null;
+
+			var typeIdent = typeIdentifiers.First();
+
+			if (itemProvider.HasItemConformingTo(typeIdent))
+				return await itemProvider.LoadInPlaceFileRepresentationAsync(typeIdent);
+
+			typeIdentifiers.Remove(typeIdent);
+			return await LoadItemAsync(itemProvider, typeIdentifiers);
+		}
+		#endif
+
+		string filePath = filePaths.FirstOrDefault();
+
+        // Process the dropped file
+
+        textbox.Text = filePath;
     }
 
     /// <summary>
